1fe8fc81be9794073bf0c69160325feff30ece78
Ilya Ilinich <ilya.ilinich@gmail.com>
2022-10-03 23:15:06 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-app>
+    <v-card>
+      <v-app-bar app color="#121212" dark elevation="5" outlined height="60vh">
+        <div class="d-flex align-center">
+          <v-img
+            alt="My_Logo"
+            class="shrink mr-2"
+            contain
+            src="./assets/ava.png"
+            transition="scale-transition"
+            width="40"
+          />
+        </div>
+
+        <v-spacer></v-spacer>
+
+        <span>
+          С днем учителя, Максим Кадомцев, всей группой желаем, чтобы у Вас были хорошие ученики!
+        </span>
+        
+        <v-spacer></v-spacer>
+
+        <v-btn
+          href="https://vk.com/id223595943"
+          target="_blank"
+          text
+        >
+          <span class="mr-2">Ваш вк</span>
+          <v-icon>mdi-open-in-new</v-icon>
+        </v-btn>
+      </v-app-bar>
+    </v-card>
+
+    
+    <v-main>
+      <SliderController/>
+    </v-main>
+    
+    
+    <v-app-bar
+    elevation="24" dark
+    >
+      <v-spacer></v-spacer>
+      <v-btn
+      href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+      >
+        <span>Поздравление на Youtube</span>
+
+        <v-icon>mdi-youtube</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn>
+        <span>Эта кнопка ничего не делает</span>
+
+        <v-icon>mdi-message</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn
+      href="https://www.youtube.com/watch?v=76wzB8-GB98"
+      >
+        <span>Поздравительная музыка</span>
+
+        <v-icon>mdi-spotify</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+  </v-app>
+</template>
+
+<script>
+import SliderController from './components/SliderController';
+
+export default {
+  name: 'App',
+
+  components: {
+    SliderController,
+},
+
+  data: () => ({
+    value: 'recent'
+  }),
+};
+</script>
+
+<style>
+  main { 
+    background-color: #000000; 
+  }
+</style>
---
+++ src/components/SliderController.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-carousel
+    height="95vh"
+    hide-delimiters
+  >
+    <v-carousel-item
+      v-for="(item, i) in items"
+      :key="i"
+      :src="item.src"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    >
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+  
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: "https://cdn.otkritkiok.ru/posts/big/s-dnem-uchitelya-gif-47777-8866353.gif",
+          },
+          {
+            src: 'https://media.tenor.com/6En8e547Qb0AAAAd/gandalf-gandalf-araba.gif',
+          },
+          {
+            src: 'https://c.tenor.com/CWgfFh7ozHkAAAAC/rick-astly-rick-rolled.gif',
+          },
+          {
+            src: 'https://media.tenor.com/TbTe1Nc6j34AAAAC/hacker-hackerman.gif',
+          },
+        ],
+      }
+    },
+  }
+</script>
